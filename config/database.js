7cca3a2a139b4762a9de7a3f507102831f40fe5f
codec-akash <akashrdubey@yahoo.in>
2025-02-08 13:19:13 +0000

    pull from main

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = new Sequelize('Stocks', process.env.USER_NAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize('stocks', process.env.USER_NAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
 });
