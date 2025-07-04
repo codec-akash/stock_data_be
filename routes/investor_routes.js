190faf74a6b503816b216db3be029192cf608043
codec-akash <akashrdubey@yahoo.in>
2025-07-05 00:33:44 +0530

    added list of investors for a stock

--- routes/investor_routes.js
+++ routes/investor_routes.js
@@ -5,4 +5,7 @@ const investorController = require('../controllers/investor_controller');
 // Route to get trades for a specific investor
 router.get('/:clientName', investorController.getInvestorTrades);
 
+// Route to get investors holding a specific stock
+router.get('/', investorController.getInvestorsByStock);
+
 module.exports = router; 
